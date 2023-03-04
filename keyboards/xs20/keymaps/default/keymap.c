c5ba5d967e27f524fbbc9d4d749c87782715e244
Roming22 <roming22@gmail.com>
2023-03-04 14:19:04 -0500

    Add layer

--- keyboards/xs20/keymaps/default/keymap.c
+++ keyboards/xs20/keymaps/default/keymap.c
@@ -8,12 +8,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // Left
         KC_A,   KC_B,   KC_C,
         KC_D,   KC_E,   KC_F,
-                KC_G,   KC_H,   KC_I,
+                KC_SPC, KC_LGUI,    KC_NO,
         // Right
                 KC_J,   KC_K,   KC_L,
                 KC_M,   KC_N,   KC_O,
-        KC_P,   KC_Q,   KC_R
-    )
+        KC_NO,  TO(1),  KC_ENTER
+    ),
+    [1] = LAYOUT(
+        // Left
+        KC_ESC,     KC_PGUP,    KC_PSCR,
+        KC_HOME,    KC_PGDN,    KC_END,
+                    _______,    _______,    _______,
+        // Right
+                    KC_BSPC,    KC_UP,      KC_DEL,
+                    KC_LEFT,    KC_DOWN,    KC_RIGHT,
+        _______,    TO(0),      _______
+    ),
 };
 
 #include "combos.h"
