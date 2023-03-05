246941dd34bbdfba1ef156f5a0eaaa4e27ef93ae
Roming22 <roming22@gmail.com>
2023-03-05 16:28:50 -0500

    Right half is working

--- keyboards/xs20/keymaps/default/overrides/L1-symbols.h
+++ keyboards/xs20/keymaps/default/overrides/L1-symbols.h
@@ -7,8 +7,8 @@ const key_override_t at_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_HASH, KC
 const key_override_t ampersand_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOLLAR, KC_AMPERSAND);
 const key_override_t question_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_EXCLAIM, KC_QUESTION);
 // Right
-const key_override_t plus_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_MINUS, KC_PLUS);          // Not working
-const key_override_t asterisk_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_SLASH, KC_ASTERISK);  // Not working
-const key_override_t percent_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_EQUAL, KC_PERCENT);    // Not working
+const key_override_t plus_key_override = ko_make_basic(MOD_MASK_SHIFT, RCTL_T(KC_MINUS), KC_PLUS);
+const key_override_t asterisk_key_override = ko_make_basic(MOD_MASK_SHIFT, RALT_T(KC_SLASH), KC_ASTERISK);
+const key_override_t percent_key_override = ko_make_basic(MOD_MASK_SHIFT, RSFT_T(KC_EQUAL), KC_PERCENT);
 const key_override_t colon_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COLON);
 const key_override_t semicolon_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMMA, KC_SEMICOLON);
