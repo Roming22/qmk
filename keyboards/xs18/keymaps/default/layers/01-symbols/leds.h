ff6a54e1fef937086737ecede7dc7dd9e575b3ad
Romain Arnaud <rarnaud@redhat.com>
2024-06-29 18:40:04 +0200

    xs18: tweak colors

--- keyboards/xs18/keymaps/default/layers/01-symbols/leds.h
+++ keyboards/xs18/keymaps/default/layers/01-symbols/leds.h
@@ -1,14 +1,15 @@
 // Copyright 2023 Roming22 (@Roming22)
 // SPDX-License-Identifier: GPL-2.0-or-later
+#define HSV_ORANGEBLOOD 4, 255, 255
 
 const rgblight_segment_t PROGMEM led_layer1[] = RGBLIGHT_LAYER_SEGMENTS(
-    {2, 4, HSV_ORANGE}
+    {2, 4, HSV_ORANGEBLOOD}
 );
 const rgblight_segment_t PROGMEM led_layer4[] = RGBLIGHT_LAYER_SEGMENTS(
     {2, 2, HSV_OFF},
-    {4, 2, HSV_ORANGE}
+    {4, 2, HSV_ORANGEBLOOD}
 );
 const rgblight_segment_t PROGMEM led_layer5[] = RGBLIGHT_LAYER_SEGMENTS(
-    {2, 2, HSV_ORANGE},
+    {2, 2, HSV_ORANGEBLOOD},
     {4, 2, HSV_OFF}
 );
