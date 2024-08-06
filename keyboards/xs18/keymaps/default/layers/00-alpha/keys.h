27deb2e53d8e3b1ceb23a80ef54dfd5b2d70a11a
Roming22 <roming22@gmail.com>
2024-08-06 15:43:10 +0000

    feat: rebalance xs18 alpha layout

--- keyboards/xs18/keymaps/default/layers/00-alpha/keys.h
+++ keyboards/xs18/keymaps/default/layers/00-alpha/keys.h
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 // Alpha keys
-#define LTP0 KC_L
-#define LTR0 LALT_T(KC_D)
+#define LTP0 KC_D
+#define LTR0 LALT_T(KC_P)
 #define LTM0 LCTL_T(KC_C)
-#define LTI0 LSFT_T(KC_R)
-#define LHR0 LT(8, KC_T)
+#define LTI0 LSFT_T(KC_N)
+#define LHR0 LT(8, KC_S)
 #define LHM0 LT(5, KC_A)
 #define LHI0 LT(11, KC_E)
 #define LT00 LT(4, KC_SPACE)
@@ -16,8 +16,8 @@
 #define RT00 LT(1, KC_ENTER)
 #define RHI0 LT(12, KC_I)
 #define RHM0 LT(6, KC_O)
-#define RHR0 LT(9, KC_S)
-#define RTI0 RSFT_T(KC_H)
-#define RTM0 RCTL_T(KC_P)
-#define RTR0 RALT_T(KC_G)
-#define RTP0 KC_N
+#define RHR0 LT(9, KC_T)
+#define RTI0 RSFT_T(KC_R)
+#define RTM0 RCTL_T(KC_H)
+#define RTR0 RALT_T(KC_L)
+#define RTP0 KC_G
