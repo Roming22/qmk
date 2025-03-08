6aff7c0018361a91ab4c9516d4367fccaacfb7dd
Roming22 <roming22@gmail.com>
2025-03-08 11:11:32 -0500

    fix: change needed to upgrade QMK version

--- keyboards/uni/config.h
+++
@@ -1,3 +0,0 @@
-// Copyright 2023 jack (@waffle87)
-// SPDX-License-Identifier: GPL-2.0-or-later
-#pragma once
--- keyboards/xs18/keymaps/default/layers/key_overrides.h
+++ keyboards/xs18/keymaps/default/layers/key_overrides.h
@@ -8,7 +8,7 @@
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
     &ampersand_key_override,
     &comma_key_override,
     &delete_key_override,
